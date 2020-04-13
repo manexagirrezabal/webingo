51e2bde22949252286563bbaad68abaec507bf96
Manex Agirrezabal <manex.agirrezabal@gmail.com>
2020-04-13 19:24:31 +0200

    Bonboa zuzendu, zenbaki berdinak ez ateratzeko.

--- myscripts.js
+++ myscripts.js
@@ -59,11 +59,46 @@ function generate_table() {
   }
 }
 
+
+//https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+
+var numbers = [];
+for (var i =0; i< 90; i++) {
+  numbers.push(i);
+}
+
+
+shuffle(numbers);
+
+
 function generate_number() {
   var lekua = document.getElementById("zenbakilekua");
 
-  var zenbakia = getRndInteger(1,90);
+  var zenbakia = numbers.pop();
 
   lekua.innerHTML = zenbakia;
+
+  var ateratakoak = document.getElementById("ateratakoak");
+
+  ateratakoak.innerHTML = ateratakoak.innerHTML + " " + zenbakia.toString();
   
 }
